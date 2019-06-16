20d3c7f75b8d8e3ceaa867832acdcbe75afb0607
Jonas Zaugg <jonas.zaugg@yahoo.com>
2019-06-16 15:45:52 +0200

    Finalize SPM integration, builds, not testable

--- Module/libxml2-fuzi.h
+++
@@ -1,5 +0,0 @@
-#import <libxml2/libxml/xmlreader.h>
-#import <libxml2/libxml/xpath.h>
-#import <libxml2/libxml/xpathInternals.h>
-#import <libxml2/libxml/HTMLparser.h>
-#import <libxml2/libxml/HTMLtree.h>
--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -8,23 +9,9 @@ let package = Package(
         .library(name: "Fuzi", targets: ["Fuzi"]),
     ],
     targets: [
-        /*.systemLibrary(
-         name: "libxmlFuzi",
-         path: "Module",
-         pkgConfig: "libxml-2.0",
-         providers: [
-         .brew(["libxml2"]),
-         .apt(["libxml2-dev"])
-         ]),*/
         .target(name: "Fuzi",
-                //dependencies: ["libxmlFuzi"],
             path: "Sources",
-            /*cSettings: [
-             .headerSearchPath("$(SDKROOT)/usr/include/libxml2")
-             ],*/
-            linkerSettings: [
-                .linkedLibrary("xml2")
-            ]
+            linkerSettings: [.linkedLibrary("xml2")]
         ),
         .testTarget(name: "FuziTests",
                     dependencies: ["Fuzi"],
